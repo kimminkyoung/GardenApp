b98282a47202c34479affa298ca88aa5f63433bb
kimminkyoung <39699079+kimminkyoung@users.noreply.github.com>
2020-07-22 16:29:14 +0900

    version up, QR Track Found
    > 2019.2.6 에서 2019.3.15
    > 마커 찾는 함수 설정

---
+++ Project_GardenApp/Assets/Scripts/QRfind.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class QRfind : MonoBehaviour
+{
+    //static QRfind QRinstant;
+    TrackableBehaviour insTrack;
+    TrackableBehaviour.Status previous;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        insTrack = GetComponent<TrackableBehaviour>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckStatus(previous);
+        previous = insTrack.CurrentStatus;
+    }
+
+    public void CheckStatus(TrackableBehaviour.Status previousStatus)
+    {
+        if (insTrack.CurrentStatus == TrackableBehaviour.Status.TRACKED ||
+            insTrack.CurrentStatus == TrackableBehaviour.Status.DETECTED)
+            TrackFound();
+        if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+            insTrack.CurrentStatus == TrackableBehaviour.Status.NO_POSE)
+            TrackLost();
+    }
+
+    public void TrackFound()
+    {
+        print("my custom track found!");
+    }
+    public void TrackLost()
+    {
+        print("my custom track lost!");
+    }
+}
+
