dd714f387ea851e392e5729d6e32f96f5beb01db
kimminkyoung <39699079+kimminkyoung@users.noreply.github.com>
2020-07-24 23:23:20 +0900

    camera switch
    게임모드에서 버튼을 통해 ar모드로 스위치

---
+++ Project_GardenApp/Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public GameObject GameCamera;
+    public GameObject ArCamera;
+
+    public void Switch()
+    {
+        GameCamera.SetActive(false);
+        ArCamera.SetActive(true);
+    }
+}
