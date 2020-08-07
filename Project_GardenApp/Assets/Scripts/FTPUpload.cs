1232e779967cd67f3bbb89583e0f1609930f742e
kimminkyoung <39699079+kimminkyoung@users.noreply.github.com>
2020-08-07 18:49:54 +0900

    FTP file upload
    > 외부 서버로 ftp 파일업로드 확인

---
+++ Project_GardenApp/Assets/Scripts/FTPUpload.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.IO;
+
+public class FTPUpload : MonoBehaviour
+{
+    public Texture2D image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FTPFileUpload();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FTPFileUpload()
+    {
+        string ftpPath = "ftp://bebeamplants.cafe24.com/myform/myimage.png";
+        string id = "bebeamplants";
+        string pwd = "bebeam2020";
+
+        FileInfo myInfo = null;
+
+        FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpPath);
+        request.Method = WebRequestMethods.Ftp.UploadFile;
+        request.Credentials = new NetworkCredential(id, pwd);
+        request.UseBinary = true;
+        Stream ftpStream = request.GetRequestStream();
+
+        byte[] data = new byte[image.EncodeToPNG().Length];
+        data = image.EncodeToPNG();
+
+        ftpStream.Write(data, 0, data.Length);
+        ftpStream.Close();
+
+       // int writeLength = FileStream.re
+
+    }
+}
