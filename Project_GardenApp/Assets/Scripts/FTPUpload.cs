d204a89ba2dccb1b72f13df5f0520542bfa1a51a
kimminkyoung <39699079+kimminkyoung@users.noreply.github.com>
2020-08-31 03:40:07 +0900

    create folder and save
    이름 받기 구현
    폴더별로 저장 구현
    
    > 호스팅이 끊겨서 테스트를 못함
    > 테스트후 다운로드 구현 필요

--- Project_GardenApp/Assets/Scripts/FTPUpload.cs
+++ Project_GardenApp/Assets/Scripts/FTPUpload.cs
@@ -3,57 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Net;
 using System.IO;
+using UnityEngine.UI;
 
 public class FTPUpload : MonoBehaviour
 {
-    public Texture2D image;
-    public Texture2D[] plantTextures;
+    public InputField getName;
     public GameObject[] plantObjects;
+
+    Texture2D[] plantTextures;
 
     private void OnEnable()
     {
         print("ftp upload script is opened");
     }
-    // Start is called before the first frame update
-    void Start()
+
+    public void GetName()
     {
+        string myName = getName.text;
+        GetObjectsTexture(myName);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
-    public void GetObjectsTexture()
+    public void GetObjectsTexture(string name)
     {
         plantTextures = new Texture2D[plantObjects.Length];
         for (int i = 0; i < plantObjects.Length; i++)
         {
-            //plantTextures[i] = plantObjects[i].GetComponent<>
+            plantTextures[i] = (Texture2D)plantObjects[i].GetComponent<MeshRenderer>().material.mainTexture;
         }
+        FTPFileUpload(name, plantTextures);
     }
 
-    public void FTPFileUpload()
+    public void FTPFileUpload(string name, Texture2D[] textures)
     {
-        string ftpPath = "ftp://bebeamplants.cafe24.com/myform/myimage.png";
-        //개인별로 구분되는 이름필요, 폴더생성 필요
-        string id = "bebeamplants";
-        string pwd = "bebeam2020";
+        for (int i = 0; i < textures.Length; i++)
+        {
+            string ftpPath = "ftp://bebeamplants.cafe24.com/myform/" + name + "Folder/" + name + i + ".png";
+            //개인별로 구분되는 이름필요, 폴더생성 필요
+            string id = "bebeamplants";
+            string pwd = "bebeam2020";
 
-        //FileInfo myInfo = null;
+            //FileInfo myInfo = null;
 
-        FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpPath);//웹서버로 올리기 위한 request생성
-        request.Method = WebRequestMethods.Ftp.UploadFile;//
-        request.Credentials = new NetworkCredential(id, pwd);
-        request.UseBinary = true;
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpPath);//웹서버로 올리기 위한 request생성
+            request.Method = WebRequestMethods.Ftp.UploadFile;//
+            request.Credentials = new NetworkCredential(id, pwd);
+            request.UseBinary = true;
 
-        Stream ftpStream = request.GetRequestStream();//시스템 입출력에서 바이트 읽기쓰기 클래스 생성
+            Stream ftpStream = request.GetRequestStream();//시스템 입출력에서 바이트 읽기쓰기 클래스 생성
 
-        byte[] data = new byte[image.EncodeToPNG().Length];
-        data = image.EncodeToPNG();
+            byte[] data = new byte[textures[i].EncodeToPNG().Length];
+            data = textures[i].EncodeToPNG();
+            //받아온 텍스쳐 하나씩 데이터로 저장
 
-        ftpStream.Write(data, 0, data.Length);//생성된 스트림으로 데이터 쓰기
-        ftpStream.Close();
+            ftpStream.Write(data, 0, data.Length);//생성된 스트림으로 데이터 쓰기
+            ftpStream.Close();
+        }
 
+        print("Upload Complet");
     }
 }
---
+++ Project_GardenApp/Assets/Scripts/TypingName.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypingName : MonoBehaviour
+{
+    Text myName;
+    public string GetName()
+    {
+        return GetComponent<InputField>().text;
+    }
+}
