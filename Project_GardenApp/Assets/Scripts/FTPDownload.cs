76d9dfb0de10474f355fe6533d628a2abebf0c83
kimminkyoung <39699079+kimminkyoung@users.noreply.github.com>
2020-09-03 04:39:05 +0900

    FTP classify by name
    네임별로 구분하여 저장
    네임별로 다운로드후 리스트 저장
    
    > 대표네임으로 ui 생성(외부 가드너) 필요
    > 대표네임과 매칭되는 이미지 세트 저장 필요
    > 매치후 불러오기 필요

---
+++ Project_GardenApp/Assets/Scripts/FTPDownload.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+public class FTPDownload : MonoBehaviour
+{
+    //1. get list
+    //2. _를 기준으로 분류
+    //3. 선택한 네임을 받아와서 리스트분류셑과 비교
+    //4. 해당되는 파일 다운로드 
+
+    public void FTPGetList()
+    {
+        string ftpPath = "ftp://bebeamplants.cafe24.com/myform/";
+        string id = "bebeamplants";
+        string pwd = "bebeam2020";
+
+        FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpPath);
+        request.Credentials = new NetworkCredential(id, pwd);
+        request.Method = WebRequestMethods.Ftp.ListDirectory;
+        FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+
+        List<string> nameList = new List<string>();
+        string line = reader.ReadLine();
+
+        while (!string.IsNullOrEmpty(line))
+        {
+            nameList.Add(line);
+            line = reader.ReadLine();
+
+            print("added line is " + line);
+        }
+
+        reader.Close();
+        response.Close();
+
+        SeparateName(nameList);
+    }
+
+    public void SeparateName(List<string> nameList)
+    {
+        List<string> name = new List<string>();
+        for (int i = 0; i < nameList.Count; i++)
+        {
+            if (Regex.IsMatch(nameList[i], "_") && Regex.IsMatch(nameList[i], "0"))
+            {
+                var voca = Regex.Split(nameList[i], "_");
+                name.Add(voca[0]);
+
+                print("splited is " + voca[0]);
+            }
+        }
+    }
+
+    public void FTPFileDownload()
+    {
+        
+    }
+}
--- Project_GardenApp/Assets/Scripts/FTPUpload.cs
+++ Project_GardenApp/Assets/Scripts/FTPUpload.cs
@@ -35,74 +35,56 @@ public class FTPUpload : MonoBehaviour
 
     public void FTPFileUpload(string name, Texture2D[] textures)
     {
-        string ftpPathFolder = "ftp://bebeamplants.cafe24.com/" + name + "Folder/";
-        string id = "bebeamplants";
-        string pwd = "bebeam2020";
-
-        FtpWebRequest requestFolder = (FtpWebRequest)WebRequest.Create(ftpPathFolder);//웹서버로 올리기 위한 request생성
-        requestFolder.UseBinary = true;
-        requestFolder.UsePassive = true;
-        requestFolder.Method = WebRequestMethods.Ftp.MakeDirectory;
-        requestFolder.Credentials = new NetworkCredential(id, pwd);
-
-        FtpWebResponse responseFolder = (FtpWebResponse)requestFolder.GetResponse();
-        responseFolder.Close();
-        //requestFolder.UsePassive = true;
-        //requestFolder.UseBinary = true;
-
-        /*
-        try
-        {
-            using(var resp = (FtpWebResponse)requestFolder.GetResponse())
-            {
-                resp.Close();
-            }
-        }catch(WebException e)
-        {
-            print("get error (^0-) ㅗ");
-            Debug.LogError(e.Message);
-        }*/
-
-        //var resp = (FtpWebResponse)requestFolder.GetResponse();
-        //resp.Close();
-
-        //Stream ftpStreamFolder = requestFolder.GetRequestStream();
-        //ftpStreamFolder.Close();
-
-
-
-        /*
+        //파일 업로드
         for (int i = 0; i < textures.Length; i++)
         {
-            // 1. 폴더생성
-            //string ftpPath = "ftp://bebeamplants.cafe24.com/myform/" + name + "Folder/" + name + i + ".png";
-            string ftpPathFolder = "ftp://bebeamplants.cafe24.com/myform/" + name + "Folder/";
+            // 1. 파일 위치, 이름 설정
+            string ftpPath = "ftp://bebeamplants.cafe24.com/myform/" + name + "_image" + i + ".png";
             string id = "bebeamplants";
             string pwd = "bebeam2020";
 
-            FtpWebRequest requestFolder = (FtpWebRequest)WebRequest.Create(ftpPathFolder);//웹서버로 올리기 위한 request생성
-            requestFolder.Credentials = new NetworkCredential(id, pwd);
-            requestFolder.Method = WebRequestMethods.Ftp.MakeDirectory;
-
-            Stream ftpStreamFolder = requestFolder.GetRequestStream();
-            ftpStreamFolder.Close();
-
-            /
+            // 2. 웹서버로 올리기 위한 request생성
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpPath);
             request.Method = WebRequestMethods.Ftp.UploadFile;//업로드
-            request.Credentials = new NetworkCredential(id, pwd);
-            request.UseBinary = true;
+            request.Credentials = new NetworkCredential(id, pwd);//아이디,비번입력
+            request.UseBinary = true;//바이너리 데이터로 전송
+            request.UsePassive = true;//방화벽사용시 자동포트로 연결
 
             Stream ftpStream = request.GetRequestStream();//시스템 입출력에서 바이트 읽기쓰기 클래스 생성
 
             byte[] data = new byte[textures[i].EncodeToPNG().Length];
             data = textures[i].EncodeToPNG();
-            //받아온 텍스쳐 하나씩 데이터로 저장
+            //하나씩 텍스쳐를 png로 데이터로 저장
 
             ftpStream.Write(data, 0, data.Length);//생성된 스트림으로 데이터 쓰기
             ftpStream.Close();
-            /
-        }*/
+        }
 
         print("Upload Complet");
     }
 }
+
+//폴더 생성후 업로드
+/*
+string ftpPathFolder = "ftp://bebeamplants.cafe24.com/myform/";// + name + "Folder/";
+string id = "bebeamplants";
+string pwd = "bebeam2020";
+
+FtpWebRequest requestFolder = (FtpWebRequest)WebRequest.Create(ftpPathFolder);//웹서버로 올리기 위한 request생성
+requestFolder.UseBinary = true;
+requestFolder.UsePassive = true;
+requestFolder.Method = WebRequestMethods.Ftp.MakeDirectory;
+requestFolder.Credentials = new NetworkCredential(id, pwd);
+
+FtpWebResponse responseFolder = (FtpWebResponse)requestFolder.GetResponse();
+responseFolder.Close();
+//requestFolder.UsePassive = true;
+//requestFolder.UseBinary = true;
+
+
+//var resp = (FtpWebResponse)requestFolder.GetResponse();
+//resp.Close();
+
+//Stream ftpStreamFolder = requestFolder.GetRequestStream();
+//ftpStreamFolder.Close();
+*/
